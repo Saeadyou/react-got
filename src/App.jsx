94a76dce6e5782a0eaeb3a2083e413c4b80e9f74
saeadyou <saeadyou@gmail.com>
2024-03-06 14:25:32 +0100

    add lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Houses from "./pages/Houses";
-import Persons from "./pages/Persons";
-import Quotes from "./pages/Quotes";
-import House from "./pages/House";
-import Person from "./pages/Person";
 import { PersonsProvider } from "./contexts/PersonsContext";
+
+const Houses = lazy(() => import("./pages/Houses"));
+const House = lazy(() => import("./pages/House"));
+const Persons = lazy(() => import("./pages/Persons"));
+const Person = lazy(() => import("./pages/Person"));
+const Quotes = lazy(() => import("./pages/Quotes"));
 
 function App() {
   return (
     <PersonsProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Houses />}>
-            <Route path="houses/:slug" element={<House />} />
-          </Route>
-          <Route path="persons" element={<Persons />}>
-            <Route path=":person" element={<Person />} />
-          </Route>
-          <Route path="quotes" element={<Quotes />} />
-        </Routes>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<Houses />}>
+              <Route path="houses/:slug" element={<House />} />
+            </Route>
+            <Route path="persons" element={<Persons />}>
+              <Route path=":person" element={<Person />} />
+            </Route>
+            <Route path="quotes" element={<Quotes />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </PersonsProvider>
   );
